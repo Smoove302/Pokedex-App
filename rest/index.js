3a5f73ed5392f9061c8b8309949a8830392e1de2
AntsmithCode <hey@antsmith.dev>
2021-02-08 13:15:33 -0500

    (API) Working API Inside Express

--- rest/index.js
+++ rest/index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fetch = require("node-fetch"); // npm install node-fetch
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const api = "https://pokeapi.co/api/v2/pokemon/1"; //store API URL to API
+const port = 3000;
+
+app.use(cors());
+
+app.get("/pokemon/search", async (req, res) => {
+  //async function
+  let resp = await fetch(api);
+  let data = await resp.json();
+  console.log(data); //console.log the data from the API
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Pokemon app listening at http://localhost:${port}`);
+});
---
+++ web/index.js
@@ -0,0 +1,11 @@
+const localhost = "http://localhost:3000";
+const btn = document.querySelector(".btn");
+
+//On Click function
+async function searchPoke() {
+  const response = await fetch(localhost);
+  const json = await response.json();
+  console.log(json.data);
+}
+
+btn.addEventListener("click", searchPoke);
